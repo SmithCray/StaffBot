4eb3f09d6e0e1b12f9c41faf23e7b2e6d1231174
SmithCray <cmsmith004@gmail.com>
2021-12-06 21:49:57 -0700

    Readme update, prompts structure

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# StaffBot
-Take in information about employees on a given team, and display summaries for each person.
+# Welcome to **StaffBot**
+
+## Table of Contents:
+
+#
+
+- Our Goal (Description)
+-
+-
+- Contact Us
+- Copyright
+
+<br>
+<br>
+<br>
+<br>
+
+## Our Goal:
+
+_"StaffBot"_ is a simple Node.js command-line application that takes user input and information about their software engineering team, then generates a HTML webpage including a summary for each person added to the team. Our goal is to provide a simple easy and intuitive experience for the user and create an appealing and personalized product using the information they have provided. When generating the HTML we want to create something easy to understand, simple to read, and accurate. _"StaffBot"_ can be used to easily log, record, and visualize your team all in one place with as much or little information as you desire making it simple to see your team and it's strengths or weaknesses.
+
+<br>
+<br>
+
+## Contact Us
+
+#
+
+Email:
+
+- SmithCray@gmail.com
+
+GitHub:
+
+- https://github.com/SmithCray
+
+<br>
+<br>
+
+## Copyright (c) [2021] [Cray Smith]
+
+#
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- index.js
+++ index.js
@@ -1,10 +1,83 @@
-// Notes:
-// Try to:
-// Keywords to use/ research
+const inquirer = require("inquirer");
+const fs = require("fs");
 
+const employee = require("./lib/Employee");
+const engineer = require("./lib/Engineer");
+const intern = require("./lib/Intern");
+const manager = require("./lib/Manager");
+
+const managerPrompt = [
+  {
+    type: "input",
+    message: "Please enter Team Manager's name.",
+    name: "managerName",
+  },
+  {
+    type: "input",
+    message: "Please enter your employee ID.",
+    name: "managerID",
+  },
+  {
+    type: "input",
+    message: "Please enter your email address.",
+    name: "managerEmail",
+  },
+  {
+    type: "input",
+    message: "Please enter you office number.",
+    name: "managerOffice",
+  },
+];
+
+const addMembers = [
+  {
+    type: "list",
+    message: "Would you like too...",
+    name: "teamMembers",
+    options: [
+      "Add an engineer.",
+      "Add an intern.",
+      "Or finish building my Team!",
+    ],
+  },
+];
+
+const engineerPrompt = [{}];
+
+const internPrompt = [{}];
+
+function buildTeam();
 //Step 1
-//Prompt to enter the team MANAGER's name, employee ID, email address, dand office number
+//Prompt to enter the team MANAGER's name, employee ID, email address, and office number
 //use inquirer npm to prompt questions
+
+// function writeToFile(fileName, data) {
+//   fs.writeFileSync(fileName, data, (err) =>
+//     err ? console.log(err) : console.log("Writing ReadMe!")
+//   );
+// }
+
+function init() {
+  inquirer
+    .prompt(managerPrompt)
+    .then((data) => {
+      //   console.log(managerPrompt(data));
+      //   var managerData = managerPrompt(data);
+      //   writeToFile("readme.md", rawMarkdown);
+      console.log(data);
+    })
+    // .then(buildTeam())
+    .catch((error) => {
+      if (error.isTtyError) {
+        console.log("Prompt couldn't be rendered in the current environment");
+      } else {
+        console.log("Something else went wrong");
+      }
+    });
+}
+
+init();
+
 //.then(
 //Call the function in step 2
 //)
@@ -29,4 +102,3 @@
 //Create a function to build the team
 //Find a way to collect all user's input for manager, Engineer, and intern
 //Use that collection to build the team html
- 
